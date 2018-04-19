5edb3d89ab7be1c56758777811ee0e66924e08e1
zainasaed <zainushka11@gmail.com>
2018-04-19 23:40:15 +0300

    نن

---
+++ src/pages/tableegh/tableegh.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TableeghPage } from './tableegh';
+
+@NgModule({
+  declarations: [
+    TableeghPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TableeghPage),
+  ],
+})
+export class TableeghPageModule {}
